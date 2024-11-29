5e09925c46e27416a85877dd9a262979b0cce054
Rob Gagne <rob@fluxweave.com>
2024-11-29 16:25:33 +0900

    better singleton

--- lib/src/stub_interface.dart
+++ lib/src/stub_interface.dart
@@ -14,8 +14,8 @@ enum HealthConnectSdkStatus { stub }
 class Health {
 
   /// The singleton [Health] instance.
-  Health._();
-  static Health _instance = Health._();
+  Health._singleton();
+  static Health _instance = Health._singleton();
   static Health get instance => _instance;
 
 
